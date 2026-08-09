fb013c8ccbb48daa1746ce9bbc41283772a3a08a
Brabian-Perez <perezvelezbrahiandavid@gmail.com>
2026-08-09 15:39:56 -0500

    se crea la clase de movimientos

---
+++ src/main/java/com/banco/miplata/models/Movimiento.java
@@ -0,0 +1,51 @@
+package com.banco.miplata.models;
+
+import com.banco.miplata.enums.TipoMovimiento;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "movimiento")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Movimiento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "tipo", nullable = false, length = 20)
+    private Long Id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipo", nullable = false, length = 20)
+    private TipoMovimiento tipo;
+
+    @Column(name = "valor",nullable = false, precision = 14,scale = 2)
+    private BigDecimal valor;
+
+    @Column(name = "saldo_posterior", nullable = false, precision = 14, scale = 2)
+    private BigDecimal saldoPosterior;
+
+    @Column(name = "fecha_hora", nullable = false, updatable = false)
+    private LocalDateTime fechaHora;
+
+    @Column(name = "descripcion", length = 255)
+    private String descripcion;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_cuenta", nullable = true)
+    private Cuenta cuenta;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_tarjeta", nullable = true)
+    private TarjetaCredito tarjeta;
+
+    @PrePersist
+    protected void onCreate() {
+        this.fechaHora = LocalDateTime.now();
+    }
+}
