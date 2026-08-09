8192dec0a1623cc8a62f6b77c4d401ac8e2b2a7a
Brabian-Perez <perezvelezbrahiandavid@gmail.com>
2026-08-09 13:07:28 -0500

    se crea el contenido subclase de CuentaAhorros

--- src/main/java/com/banco/miplata/models/CuentaAhorros.java
+++ src/main/java/com/banco/miplata/models/CuentaAhorros.java
@@ -1,4 +1,37 @@
 package com.banco.miplata.models;
 
-public class cuenta_ahorros {
+import jakarta.persistence.*;
+import org.springframework.cglib.core.Local;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "cuenta_ahorros")
+@DiscriminatorValue("AHORROS")
+@PrimaryKeyJoinColumn(name = "id_cuenta")
+public class CuentaAhorros extends Cuenta{
+
+    @Column(name = "tasa_interes_mensual", nullable = false, precision = 5, scale = 4)
+    private BigDecimal tasaInteresMensual = new BigDecimal("0.0150");
+
+    @Column(name = "fecha_ultimo_interes")
+    private LocalDateTime fechaUltimoInteres;
+
+    public BigDecimal getTasaInteresMensual(){
+        return tasaInteresMensual;
+    }
+
+    public void setTasaInteresMensual (BigDecimal tasaInteresMensual){
+        this.tasaInteresMensual=tasaInteresMensual;
+    }
+
+    public LocalDateTime getFechaUltimoInteres (){
+        return fechaUltimoInteres;
+    }
+
+    public void setFechaUltimoInteres(LocalDateTime fechaUltimoInteres){
+        this.fechaUltimoInteres = fechaUltimoInteres;
+    }
+
 }
