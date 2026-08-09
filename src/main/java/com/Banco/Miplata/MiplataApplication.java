7e48e000d092b17aa49c08ab53c34581a048ae4c
Brabian-Perez <perezvelezbrahiandavid@gmail.com>
2026-08-08 21:38:21 -0500

    Commit initial

---
+++ src/main/java/com/Banco/Miplata/MiplataApplication.java
@@ -0,0 +1,13 @@
+package com.Banco.Miplata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiplataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiplataApplication.class, args);
+	}
+
+}
